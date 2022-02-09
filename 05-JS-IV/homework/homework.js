73a87526fbafabbf4ac91e1c10160a89ed452ef5
gastonfuentes <gastonnicolasfuentes@gmail.com>
2022-02-08 22:18:09 -0300

    ejercicios JSIV resuletos

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -81,7 +81,8 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
-  if( propiedad == objeto.propiedad) return true;
+
+  if(objeto.hasOwnProperty(propiedad)) return true;
   return false;
 }
 
@@ -151,12 +152,14 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
 
+
   producto['calcularPrecioDescuento'] = function(){
-    var descuento;
-    descuento = producto.precio - (producto.precio * producto.porcentajeDescuento);
-    return descuento
+    
+    return this.precio - this.precio * this.porcentajeDeDescuento;
+    
   }
 
+ return producto; 
 }
 
 // No modificar nada debajo de esta línea
